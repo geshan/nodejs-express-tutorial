b38d4a7f62f96712fc14212e85e3175da16cf4cb
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-05-17 22:28:54 +1000

    Add the working app with pug templates, css, and js
    - [x] Theme from: https://html5up.net/eventually
    - [x] Photos of headphones from Pixabay and UnSplash
    - [x] Remove saas files

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || '8081';
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render(
+    'index', 
+    { 
+      title: 'Coming Soon!', 
+      mainText: 'Eventually Podcast', 
+      subText: `Drop you email address below and we will let you know when we launch the Eventually podcast. 
+      <br>Brought to you by amazing people`
+    }
+    );
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
