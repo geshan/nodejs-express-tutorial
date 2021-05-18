25a19ca2631350a2ed38f8983201d39b1ad38960
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-05-18 22:36:22 +1000

    Extra blank lines

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 const port = process.env.PORT || '3000';
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
