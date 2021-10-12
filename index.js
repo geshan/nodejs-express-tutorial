3eadddb8ed2f265766f82c49809201b4285e8404
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-10-12 22:16:59 +1100

    Add feature flag for footer icons with Flagsmith and cache

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const flagsmith = require('flagsmith-nodejs');
+const nodecache = require('node-cache');
 
 const app = express();
 const port = process.env.PORT || '3000';
@@ -8,16 +10,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+flagsmith.init({
+  environmentID: process.env.FLAGSMITH_ENVIRONMENT_ID || 'LKfXyih5yqZxL3huZ5LraP',
+  cache: new nodecache({stdTTL : 10, checkperiod: 10}),
+});
+
+app.get('/', async (req, res) => {
+  let showFooterIcons = false;
+  try {
+    showFooterIcons = await flagsmith.hasFeature('show_footer_icons');
+  } catch (e) {
+    console.log(`Error connecting to flagsmith - ${e.getMessage} `, e);
+  }
+
+  console.log(`show footer icons: ${showFooterIcons}`);
   res.render(
     'index', 
     { 
       title: 'Coming Soon!', 
       mainText: 'Eventually Podcast', 
       subText: `Drop your email address below and we will let you know when we launch the Eventually podcast. 
-      <br>Brought to you by amazing people`
+      <br>Brought to you by amazing people`,
+      showFooterIcons,
     }
-    );
+  );
 });
 
 app.listen(port, () => {
