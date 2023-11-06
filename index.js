de1ff761d9d9790a866c0d985d014394bfdbff5c
Novak Zaballa <zaballa.novak@gmail.com>
2023-11-06 15:12:04 -0400

    Add feature flag for footer icons with Flagsmith 3.2.0  and cache

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const NodeCache = require('node-cache');
+const Flagsmith = require('flagsmith-nodejs');
+
 
 const app = express();
 const port = process.env.PORT || '3000';
@@ -7,15 +10,34 @@ const port = process.env.PORT || '3000';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
+const myCache = new NodeCache( { stdTTL: 10, checkperiod: 10 } );
 
-app.get('/', (req, res) => {
+const flagsmith = new Flagsmith({ 
+  environmentKey: process.env.FLAGSMITH_ENVIRONMENT_ID || 'NowEDzKzNJXZVTVanLVdMQ',
+  cache: myCache,
+  
+})
+
+app.get('/', async (req, res) => {
+  let showFooterIcons = false;
+  const flags = await flagsmith.getEnvironmentFlags();
+
+  try {
+    showFooterIcons = flags.isFeatureEnabled("show_footer_icons")
+    console.log('DEBUG: showFooterIcons:', showFooterIcons)
+  } catch (e) {
+    console.log(`Error connecting to flagsmith - ${e.getMessage} `, e);
+  }
+
+
   res.render(
     'index', 
     { 
       title: 'Coming Soon!', 
       mainText: 'Eventually Podcast', 
       subText: `Drop your email address below and we will let you know when we launch the Eventually podcast. 
-      <br>Brought to you by amazing people`
+      <br>Brought to you by amazing people`,
+      showFooterIcons,
     }
     );
 });
@@ -23,3 +45,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
+
